c7c9e3e5f5ae918e3425887f7b8997ecdf7fbc79
SAHIL RAJA <sraza1692@gmail.com>
2025-08-21 08:52:57 +0530

    Add files via upload

---
+++ maxSubArraySum.java
@@ -0,0 +1,34 @@
+ 
+ import java.util.*;
+// prefix array for subarray sum
+public class maxSubArraySum{
+    // prefix array for subarray sum
+
+public static void subarraySum(int numbers[]){
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    int prefix[] = new int[numbers.length];
+    
+    prefix[0] = numbers[0];
+    for(int i=1; i<prefix.length; i++ ){
+        prefix[i] = prefix[i-1] + numbers[i];
+    }
+    // calculate prefix array 
+    for(int i=0; i<numbers.length; i++){
+        int start = i;
+        for(int j=i; j<numbers.length; j++){
+            int end = j;
+            currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
+            if(maxSum < currSum ){
+                maxSum = currSum ; 
+            }
+        }
+    }
+    System.out.println("max sum = " +maxSum);
+}
+public static void main(String args[]){
+    int numbers[] = {1, 2, -3, -1, 6, 5};
+    subarraySum(numbers);
+ }
+}
